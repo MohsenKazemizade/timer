3a5266a8da1ba1358878ac254a009394a10ee04e
MohsenKazemizade <mohsenkazemizade.dev@gmail.com>
2024-12-28 13:09:32 +0330

    create an input to control the value of the timer

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var timerBTN = document.getElementById("timer");
+var inputElement = document.getElementById("custom-number");
 
-function countDown(countStart) {
+function countDown() {
+  console.log(inputElement.value);
+  if (!inputElement.value || inputElement.value == 0) {
+    countStart = 120;
+  } else {
+    countStart = Math.abs(inputElement.value);
+  }
+  inputElement;
   timerBTN.classList.add("timer");
   timerBTN.disabled = true;
+  inputElement.disabled = true;
   timerBTN.textContent =
     String(Math.floor(countStart / 60)) +
     " : " +
@@ -19,10 +28,12 @@ function countDown(countStart) {
       timerBTN.textContent = "ارسال پیام";
       timerBTN.classList.remove("timer");
       timerBTN.disabled = false;
+      inputElement.disabled = false;
+      inputElement.value = "";
       clearInterval(countDownTimer);
     }
   }, 1000);
 }
 timerBTN.addEventListener("click", function () {
-  countDown(120);
+  countDown();
 });
